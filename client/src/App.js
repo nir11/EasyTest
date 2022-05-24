21b843fcc356a1253a47b7992c244c0df35aae42
nir-hiteach <76162225+nir-hiteach@users.noreply.github.com>
2022-05-24 10:52:02 +0300

    change /Location route to /recommended

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,25 @@
-import './App.css';
+import "./App.css";
 import { Routes, Route, HashRouter } from "react-router-dom";
 
 //pages
-import Home from './Pages/Home/Home';
-import Location from './Pages/Location/Location';
-import Summary from './Pages/Summary/Summary';
+import Home from "./Pages/Home/Home";
+import Location from "./Pages/Location/Location";
+import Summary from "./Pages/Summary/Summary";
 
 //components
-import Nav from './components/Nav/Nav'
+import Nav from "./components/Nav/Nav";
 
 function App() {
   return (
-    <div className='rootBackround'>
+    <div className="rootBackround">
       <Nav />
       <Routes>
         <Route path="/home" element={<Home />} />
-        <Route path="/location/" element={<Location />} />
+        <Route path="/recommended/" element={<Location />} />
         <Route path="/appointment-saved/" element={<Summary />} />
-        <Route path='*' element={<Home />} />
+        <Route path="*" element={<Home />} />
       </Routes>
-
-
-    </div >
+    </div>
   );
 }
 
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -1,44 +1,49 @@
-import React, { useState } from 'react'
-import { Nav } from 'react-bootstrap'
-import { useNavigate } from 'react-router-dom'
-import './nav.css'
+import React, { useState } from "react";
+import { Nav } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import "./nav.css";
 
 const MyNav = () => {
+  const [isActive1, setIsActive1] = useState(false);
+  const [isActive2, setIsActive2] = useState(false);
 
-    const [isActive1, setIsActive1] = useState(false)
-    const [isActive2, setIsActive2] = useState(false)
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const pushTo = (path) => {
+    navigate(path);
+  };
+  return (
+    <Nav activeKey="/">
+      <Nav.Item>
+        <Nav.Link
+          onClick={() => {
+            setIsActive1(!isActive1);
+            setIsActive2(false);
+            pushTo("/home");
+          }}
+          className={isActive1 ? "active-nav-item" : ""}
+        >
+          קביעת תור &nbsp;<i className="fas fa-calendar-check"></i>
+        </Nav.Link>
+      </Nav.Item>
 
-    const pushTo = (path) => {
-        navigate(path)
-    }
-    return (
-        <Nav activeKey="/">
+      <Nav.Item>
+        <Nav.Link
+          onClick={() => {
+            setIsActive1(false);
+            setIsActive2(!isActive2);
+            pushTo("/recommended");
+          }}
+          className={isActive2 ? "active-nav-item" : ""}
+        >
+          {" "}
+          המלצה לפי מיקום &nbsp;<i className="fas fa-map-marker-alt"></i>
+        </Nav.Link>
+      </Nav.Item>
 
-            <Nav.Item>
-                <Nav.Link onClick={() => {
-                    setIsActive1(!isActive1)
-                    setIsActive2(false)
-                    pushTo("/home")
-                }} className={isActive1 ? "active-nav-item" : ""}>קביעת תור
-                    &nbsp;<i className="fas fa-calendar-check"></i>
-                </Nav.Link>
-            </Nav.Item>
+      <h2>EasyTest</h2>
+    </Nav>
+  );
+};
 
-            <Nav.Item>
-                <Nav.Link onClick={() => {
-                    setIsActive1(false)
-                    setIsActive2(!isActive2)
-                    pushTo("/location")
-                }} className={isActive2 ? "active-nav-item" : ""}> המלצה לפי מיקום
-                    &nbsp;<i className="fas fa-map-marker-alt"></i>
-                </Nav.Link>
-            </Nav.Item>
-
-            <h2>EasyTest</h2>
-        </Nav >
-    )
-}
-
-export default MyNav
+export default MyNav;
