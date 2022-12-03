74d1d4bb3305f87cc35d79c261395d7f305df233
shay-hiteach <76162225+nir-hiteach@users.noreply.github.com>
2022-12-03 13:57:16 +0200

    build client

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const Api = axios.create({
   // baseURL: "http://localhost:4000/",
-  baseURL: "/api",
-  // baseURL: "https://easy-test-israel.herokuapp.com/api",
+  // baseURL: "/api",
+  baseURL: "https://easy-test-israel.herokuapp.com/api",
 });
 export default Api;
