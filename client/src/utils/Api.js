b8ed7d338c9cb65088383a565e2ad0c38920d301
shay-hiteach <76162225+nir-hiteach@users.noreply.github.com>
2022-12-19 20:51:41 +0200

    build client

--- client/src/utils/Api.js
+++ client/src/utils/Api.js
@@ -3,6 +3,6 @@ import axios from "axios";
 const Api = axios.create({
   // baseURL: "http://localhost:4000/",
   // baseURL: "/api",
-  baseURL: "https://easy-test-israel.netlify.app",
+  baseURL: "https://easytest.onrender.com",
 });
 export default Api;
