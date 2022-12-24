1462e8b3521bdaef68d5ebcdff820a0f9ee0b088
shay-hiteach <76162225+nir-hiteach@users.noreply.github.com>
2022-12-24 21:28:13 +0200

    fix home header

--- client/src/Pages/Home/Home.js
+++ client/src/Pages/Home/Home.js
@@ -3,15 +3,16 @@ import { MDBAnimation } from "mdbreact";
 
 import AppointmentForm from "../../components/AppointmentForm/AppointmentForm";
 import { useParams } from "react-router-dom";
+import "./home.scss";
 
 const Home = () => {
   const { editAppointmentId } = useParams();
 
   return (
-    <div className="container-fluid">
+    <div className="container-fluid home-container">
       <div className="container">
         <MDBAnimation type="fadeIn" delay="0.5s">
-          <div className="row">
+          <div className="row home-header">
             <div className="col-lg-7" style={{ margin: "auto" }}>
               <h1>{editAppointmentId ? "עריכת" : "קביעת"} תור למבחן רישוי</h1>
               <br />
